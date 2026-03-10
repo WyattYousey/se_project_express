aa2755fc53f8cae2d22a850e575a29e4108ae189
wyattYousey <quiz.nv1@gmail.com>
2026-03-10 13:53:27 -0500

    Small tweak to schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,6 +19,36 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: [true, "The email field is required"],
+    validate: {
+      validator(value) {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(value);
+      },
+      message: "You must enter a valid Email address",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxLength: 25,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10); // 10 salt rounds are a good default
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = mongoose.model("user", userSchema);
