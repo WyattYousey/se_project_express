729835ae7e047ea0bfc37faa9ba99c0b33552f72
wyattYousey <quiz.nv1@gmail.com>
2026-03-11 17:27:06 -0500

    Bug Fix 0.1.17
    making github actions happy :(

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ mongoose
 app.use(cors());
 
 app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
 
 app.use("/", mainRouter);
 
