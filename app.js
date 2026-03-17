6a09f70f91ab44e97c5cd6113567a7eb2f386894
wyattYousey <quiz.nv1@gmail.com>
2026-03-17 14:52:11 -0500

    Update connection string due to need for Atlas

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose
-  .connect("mongodb://localhost:27017/wtwr_db")
+  .connect(
+    "mongodb+srv://dbquiz-1:QWQLCAZ2ogwHFUMu@cluster0.q0ijuih.mongodb.net/?appName=Cluster0"
+  )
   .then(() => {
     console.log("Connected to DB");
   })
